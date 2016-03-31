0ba3ba94daa6664483c813398a037082f401ac5d
swarajsaaj <swarajpalsaaj@yahoo.co.in>
2016-03-31 18:41:59 +0530

    Completed UI

---
+++ app/src/main/java/io/github/swarajsaaj/retrofitdemo/models/Post.java
@@ -0,0 +1,69 @@
+package io.github.swarajsaaj.retrofitdemo.models;
+
+/**
+ * Created by swarajpal on 31-03-2016.
+ */
+public class Post {
+
+    private Integer userId;
+    private Integer id;
+    private String title;
+    private String body;
+
+    /**
+     * @return The userId
+     */
+    public Integer getUserId() {
+        return userId;
+    }
+
+    /**
+     * @param userId The userId
+     */
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * @return The id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id The id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return The title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title The title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return The body
+     */
+    public String getBody() {
+        return body;
+    }
+
+    /**
+     * @param body The body
+     */
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+}
---
+++ app/src/main/java/io/github/swarajsaaj/retrofitdemo/services/PostService.java
@@ -0,0 +1,22 @@
+package io.github.swarajsaaj.retrofitdemo.services;
+
+import java.util.List;
+
+import io.github.swarajsaaj.retrofitdemo.models.Post;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by swarajpal on 28-03-2016.
+ */
+public interface PostService {
+
+    @GET("/posts")
+    Call<List<Post>> getAllPosts();
+
+    @GET("/posts/{id}")
+    Call<List<Post>> getPostById(@Path("id") int id);
+
+
+}
